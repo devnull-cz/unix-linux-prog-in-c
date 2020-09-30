b838cb645955ccde3503eae118b1552212fbc1bb
Vladimir Kotal <vlada@devnull.cz>
2020-09-30 10:04:57 +0200

    compile workflow

---
+++ dev/install-latex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install -y --no-install-recommends \
+    texlive-fonts-recommended \
+    texlive-latex-extra \
+    texlive-fonts-extra \
+    dvipng \
+    texlive-latex-recommended \
+    texlive-pstricks
