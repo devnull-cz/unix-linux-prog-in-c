c6e90070fadf56d460c33aeccd6013422183647f
Vladimir Kotal <vlada@kotalovi.cz>
2025-01-05 22:42:09 +0100

    correctly propagate the error value from the sub-shell
    also make the script more robust

--- dev/check-examples.sh
+++ dev/check-examples.sh
@@ -8,13 +8,27 @@
 # Lastly, it assumes that the \example{} does not span multiple lines.
 #
 
+SRC_DIR="../unix-linux-prog-in-c-src"
+
+if [[ ! -d $SRC_DIR ]]; then
+	echo "directory $SRC_DIR does not exist"
+	exit 1
+fi
+
+ret=0
 for m4file in *.m4.tex; do
 	echo "### $m4file"
-	cat "$m4file" | grep -o '\\example{[[:alnum:]\/\._\-]\+}' | \
-	    sort -u | sed -e 's/^\\example{//' -e 's/}$//' | while read example; do
-		if [[ ! -f ../unix-linux-prog-in-c-src/$example ]]; then
+	oldIFS=$IFS
+	cat "$m4file" | grep -o '\\example{[[:alnum:]\/\._\-]\+}' |
+	    sort -u | sed -e 's/^\\example{//' -e 's/}$//' | while IFS='\n' read example; do
+		if [[ ! -f $SRC_DIR/$example ]]; then
 			echo "example $example is bad link"
 			exit 1
 		fi
 	done
+	if (( $? != 0 )); then
+		ret=1
+	fi
+	IFS=$oldIFS
 done
+exit $ret
